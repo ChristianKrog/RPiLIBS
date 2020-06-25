ebf80df568114b496a001b0b0a234e64c43e79ad
Christian Lind Krog <chlk@linak.com>
2020-06-25 09:32:06 +0200

    asdasd

--- DirectRegisterAccesGPIO.cpp
+++ DirectRegisterAccesGPIO.cpp
@@ -1,44 +1,47 @@
-#include "DirectRegisterAccesGPIO.hpp"
-#define BCM2711_PERI_BASE 0xFE000000             //Only for RPi 4
-#define GPIO_BASE (BCM2711_PERI_BASE + 0x200000) //GPIO controller
-#define PAGE_SIZE (4 * 1024)
-#define BLOCK_SIZE (4 * 1024)
-int mem_fd;
-void *gpio_map;
+/***********************************************************************/
+/*************    Christian L. Krog & Jesper R. Nielsen    *************/
+/***********************************************************************/
+/************* gpioDRA.cpp controls the GPIO's on the RPi  *************/
+/*************        using direct register access         *************/
+/***********************************************************************/
+
+#include "gpioDRA.hpp"
+
 volatile unsigned *gpio; //I/O access
-// GPIO setup macros. Always use INP_GPIO(x) before using OUT_GPIO(x) or SET_GPIO_ALT(x,y)
+void *gpio_map;
+
+//GPIO setup macros. Always use INP_GPIO(x) before using OUT_GPIO(x) or SET_GPIO_ALT(x,y)
 #define INP_GPIO(g) *(gpio + ((g) / 10)) &= ~(7 << (((g) % 10) * 3)) //Input
 #define OUT_GPIO(g) *(gpio + ((g) / 10)) |= (1 << (((g) % 10) * 3))  //Output
 #define SET_GPIO_ALT(g, a) *(gpio + (((g) / 10))) |= (((a) <= 3 ? (a) + 4 : (a) == 4 ? 3 : 2) << (((g) % 10) * 3))
-#define GPIO_SET *(gpio + 7)                  //sets bits which are 1 ignores bits which are 0
-#define GPIO_CLR *(gpio + 10)                 //clears bits which are 1 ignores bits which are 0
+#define GPIO_SET *(gpio + 7)                  //Sets bits which are 1. Ignores bits which are 0
+#define GPIO_CLR *(gpio + 10)                 //Clears bits which are 1. Ignores bits which are 0
 #define GET_GPIO(g) (*(gpio + 13) & (1 << g)) //0 if LOW, (1<<g) if HIGH
 #define GPIO_PULL *(gpio + 37)                //Pull up/pull down
 #define GPIO_PULLCLK0 *(gpio + 38)            //Pull up/pull down clock
 
 GPIODRA::GPIODRA()
 {
-    if ((mem_fd = open("/dev/mem", O_RDWR | O_SYNC)) < 0) //open /dev/mem
+    if ((fdMemory_ = open("/dev/mem", O_RDWR | O_SYNC)) < 0) //open /dev/mem
     {
-        cout << "can't open /dev/mem " << endl;
-        exit(1);
+        perror("Error - open - gpioDRA.cpp");
+        exit(EXIT_FAILURE);
     }
 
-    /////mmap GPIO//////
     gpio_map = mmap(
         NULL,                   //Any adddress in our space will do
-        BLOCK_SIZE,             //Map length
+        c_BLOCK_SIZE,           //Map length
         PROT_READ | PROT_WRITE, //Enable reading & writting to mapped memory
         MAP_SHARED,             //Shared with other processes
-        mem_fd,                 //File to map
-        GPIO_BASE               //Offset to GPIO peripheral
+        fdMemory_,              //File to map
+        c_GPIO_BASE             //Offset to GPIO peripheral
     );
-    close(mem_fd); //No need to keep mem_fd open after mmap
+    close(fdMemory_); //No need to keep fdMemory_ open after mmap
 
     if (gpio_map == MAP_FAILED)
     {
-        cout << "mmap error " << (int)gpio_map << endl; //errno also set!
-        exit(1);
+        perror("Error - map - gpioDRA.cpp");
+        exit(EXIT_FAILURE);
     }
     gpio = (volatile unsigned *)gpio_map; //Volatile pointer. Important
 }
@@ -47,67 +50,72 @@ GPIODRA::~GPIODRA()
 {
     for (int i = 0; i < 28; i++)
     {
-        GPIO_CLR = 1 << i;
+        GPIO_CLR = 1 << i; //Clear all GPIO's. Total of 28 GPIO's on RPi 4b
     }
 }
 
-void GPIODRA::setMode(int _gpio, int direction) //0 = "IN", 1 = "OUT"
+void GPIODRA::clearGPIO(const int GPIO)
+{
+    GPIO_CLR = 1 << GPIO; //Clear GPIO
+}
+
+void GPIODRA::setModeDRA(const int GPIO, const int direction) //0 = input, 1 = output
 {
     if (direction == 0)
     {
-        INP_GPIO(_gpio);
+        INP_GPIO(GPIO); //Set selected GPIO to input mode
     }
     else if (direction == 1)
     {
-        INP_GPIO(_gpio);
-        OUT_GPIO(_gpio);
+        INP_GPIO(GPIO); //Must set GPIO to input, before setting to output
+        OUT_GPIO(GPIO); //Set selected GPIO to output mode
     }
 }
 
-void GPIODRA::write(int _gpio, int value)
+void GPIODRA::writeDRA(const int GPIO, const int value) //Value parameter must be an int datatype (1 or 0)
 {
     if (value == 0)
     {
-        GPIO_CLR = 1 << _gpio;
+        GPIO_CLR = 1 << GPIO; //Write 0 to selected GPIO
     }
     else if (value == 1)
     {
-        GPIO_SET = 1 << _gpio;
+        GPIO_SET = 1 << GPIO; //Write 1 to selected GPIO
     }
 }
 
-void GPIODRA::writeString(int _gpio, string value)
+void GPIODRA::writeDRAString(const int GPIO, const string value) //Value parameter must be a string datatype ("1" or "0")
 {
     if (value == "0")
     {
-        GPIO_CLR = 1 << _gpio;
+        GPIO_CLR = 1 << GPIO; //Write 0 to selected GPIO
     }
     else if (value == "1")
     {
-        GPIO_SET = 1 << _gpio;
+        GPIO_SET = 1 << GPIO; //Write 1 to selected GPIO
     }
 }
 
-int GPIODRA::read(int _gpio)
+int GPIODRA::readDRA(const int GPIO) //Read selected GPIO
 {
-    if (GET_GPIO(_gpio))
+    if (GET_GPIO(GPIO))
     {
-        return 1;
+        return 1; //Return integer 1
     }
     else
     {
-        return 0;
+        return 0; //Return integer 0
     }
 }
 
-string GPIODRA::readString(int _gpio)
+string GPIODRA::readDRAString(const int GPIO) //Read selected GPIO
 {
-    if (GET_GPIO(_gpio))
+    if (GET_GPIO(GPIO))
     {
-        return "1";
+        return "1"; //Return string "1"
     }
     else
     {
-        return "0";
+        return "0"; //Return string "0"
     }
 }
--- DirectRegisterAccesGPIO.hpp
+++ DirectRegisterAccesGPIO.hpp
@@ -1,23 +1,35 @@
 #pragma once
-#include <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h>
+
+#include <cstdio>
+#include <cstdlib>
 #include <sys/mman.h>
+#include <string>
 #include <unistd.h>
-#include <string.h>
-#include <iostream>
+#include <fcntl.h>
 
 using namespace std;
+
+/////GLOBAL CONSTANTS/////
+const unsigned int c_BCM2835_PERI_BASE = 0x20000000;               //Use with RPi Zero & 1 - Not used
+const unsigned int c_BCM2836_PERI_BASE = 0x3F000000;               //Use with RPi 2 - Not used
+const unsigned int c_BCM2837_PERI_BASE = 0x3F000000;               //Use with RPi 3 - Not used
+const unsigned int c_BCM2711_PERI_BASE = 0xFE000000;               //Use with RPi 4
+const unsigned int c_GPIO_BASE = (c_BCM2711_PERI_BASE + 0x200000); //GPIO controller
+const unsigned int c_PAGE_SIZE = (4 * 1024);
+const unsigned int c_BLOCK_SIZE = (4 * 1024);
 
 class GPIODRA
 {
 private:
+    int fdMemory_; //Memory file descriptor
+
 public:
-    GPIODRA();
-    ~GPIODRA();
-    void setMode(int _gpio, int direction);    //Set mode of GPIO, direction = 0 = input, direction = 1 = output
-    void write(int _gpio, int value);          //Write to GPIO, value = 0/1
-    void writeString(int _gpio, string value); //Write to GPIO, value = "0"/"1"
-    int read(int _gpio);                       //Read from GPIO, return decimal 0/1
-    string readString(int _gpio);              //Read from GPIO, return string "0"/"1"
+    GPIODRA();                                               //Initiate direct memory access
+    ~GPIODRA();                                              //Clear all GPIO ports
+    void clearGPIO(const int GPIO);                          //Clear specific GPIO port
+    void setModeDRA(const int GPIO, const int direction);    //Set mode of GPIO, direction = 0 = input, direction = 1 = output
+    void writeDRA(const int GPIO, const int value);          //Write to GPIO, value = 0/1
+    void writeDRAString(const int GPIO, const string value); //Write to GPIO, value = "0"/"1"
+    int readDRA(const int GPIO);                             //Read from GPIO, return decimal 0/1
+    string readDRAString(const int GPIO);                    //Read from GPIO, return string "0"/"1"
 };
